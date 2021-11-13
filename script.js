33804052ef8fcf108425557a99a279814c352284
Pheamma <68507144+Pheamma@users.noreply.github.com>
2021-11-13 11:10:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-function openNav()
- {
-	 document.getElementById("sideNav").style.width = "250px";
-	 document.getElementById("sideNav").style.background = "black";
- }
- function closeNav()
- {
-	 document.getElementById("sideNav").style.width = "0px";
- }
+function openNav() {
+  document.getElementById("sideNav").style.width = "250px";
+  document.getElementById("sideNav").style.background = "black";
+}
+
+function closeNav() {
+  document.getElementById("sideNav").style.width = "0px";
+}

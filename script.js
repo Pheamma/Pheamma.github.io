b8def09b67d911a237573da8145f7779cbef6f68
Pheamma <68507144+Pheamma@users.noreply.github.com>
2021-11-13 10:42:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function openNav()
  {
 	 document.getElementById("sideNav").style.width = "250px";
-	 document.getElementById("sideNav").style.background = black;
+	 document.getElementById("sideNav").style.background = "black";
  }
  function closeNav()
  {
